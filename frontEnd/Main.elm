f498c653ee117ccc32e5bdcbc1d4af4f2160b877
paperjuice <paperjuiceing@gmail.com>
2018-06-14 11:01:51 +0200

    Add elm and css header

--- frontEnd/Main.elm
+++ frontEnd/Main.elm
@@ -1,4 +1,5 @@
-import Html exposing (Html, text, div)
+import Html exposing (Html, text, div, img, button, span)
+import Html.Attributes exposing (class, src, style)
 import Navigation exposing (Location)
 import UrlParser exposing (parseHash, s, oneOf, top, map)
 
@@ -29,43 +30,73 @@ matchRoute location =
 matcher =
   oneOf
     [ map Home top
-    , map Rooms (s "/rooms")
+    , map Rooms (s "rooms")
     ]
 
 -- MODEL --
+type Language = Ro | En
 type alias Model =
   { route : Route
+  , language: Language
   }
 
 -- INIT --
 init : Location -> (Model, Cmd msg)
 init location =
-  update (Path location) (Model Home)
+  update (Path location) (Model Home Ro)
 
 
 -- VIEW --
 view : Model -> Html msg
 view model =
-  div [ ] [ text "hello" ]
+  case model.route of
+    Home     -> viewHome model
+    Rooms    -> div [ ] [ text "Rooms" ]
+    NotFound -> div [ ] [ text "Nope :(" ]
 
 
 -- UPDATE --
 update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
   case msg of
-    _-> ( model, Cmd.none )
+    Path location ->
+      let
+          newRoute = matchRoute (Debug.log "loc" location)
+      in
+          ( { model | route = newRoute }, Cmd.none )
 
 
 ---------------
 -- HOME VIEW --
 ---------------
+viewHome : Model -> Html msg
+viewHome model =
+  div [ class "home" ]
+      [ header model
+      ]
 
+header : Model -> Html msg
+header model =
+  div [ class "header" ]
+      [ div [ class "hotel-criss" ] [ text "Hotel Criss" ]
+      , img [ class "hotel-stars", src "icons/hotel-star.svg" ] [ ]
+      , button [ class "book" ] [ text "Rezerva" ]
+      , div [ class "language" ] (languageColor model.language)
+      ]
 
+languageColor : Language -> List (Html msg)
+languageColor language =
+  case language of
+    Ro ->
+      languageButtons "#4d4d4d" "white"
 
+    En ->
+      languageButtons "white" "#4d4d4d"
 
-
-
-
-
-
+languageButtons : String -> String -> List (Html msg)
+languageButtons colorRo colorEn =
+      [ button [ style [ ("color", colorRo) ] ] [ text "Ro" ]
+      , span [ ] [ text "/" ]
+      , button [ style [ ("color", colorEn) ] ] [ text "En" ]
+      ]
 
